2241cc6ddc638303e7a32f649eebb468f9bce7c9
Эмиль Барышев <01emilice01@gmail.com>
2021-05-22 14:11:05 +0300

    copy chess3 and add ntw branch

--- Chess-4/Program.cs
+++ Chess-4/Program.cs
@@ -1,12 +1,137 @@
-﻿using System;
+﻿// Baryshev Emil 220
+// Chess 3
+// 13.03.2021
+using System;
 
-namespace Chess_4
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        King test = new King(1, 1);
+        Console.WriteLine(test.Move(8, 1) ? "YES" : "NO");
+    }
+}
+
+class Figure
+{
+    protected int X;
+    protected int Y;
+
+    public Figure(int newX, int newY)
+    {
+        X = newX;
+        Y = newY;
+    }
+
+    public virtual bool Move(int newX, int newY)
+    {
+        return false;
+    }
+}
+
+class Rook : Figure
+{
+    public Rook(int newX, int newY) : base(newX, newY)
+    {
+    }
+
+    public override bool Move(int newX, int newY)
+    {
+        if (X == newX || Y == newY)
         {
-            Console.WriteLine("Hello World!");
+            X = newX;
+            Y = newY;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+class Queen : Figure
+{
+    public Queen(int newX, int newY) : base(newX, newY)
+    {
+    }
+
+    public override bool Move(int newX, int newY)
+    {
+        if (X == newX || Y == newY ||
+                 Math.Abs(X - newX) == Math.Abs(Y - newY))
+        {
+            X = newX;
+            Y = newY;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+class Bishop : Figure
+{
+    public Bishop(int newX, int newY) : base(newX, newY)
+    {
+    }
+
+    public override bool Move(int newX, int newY)
+    {
+        if (Math.Abs(X - newX) == Math.Abs(Y - newY))
+        {
+            X = newX;
+            Y = newY;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+class Knight : Figure
+{
+    public Knight(int newX, int newY) : base(newX, newY)
+    {
+    }
+
+    public override bool Move(int newX, int newY)
+    {
+        if ((Math.Abs(X - newX) == 2 && Math.Abs(Y - newY) == 1) ||
+                (Math.Abs(X - newX) == 1 && Math.Abs(Y - newY) == 2))
+        {
+            X = newX;
+            Y = newY;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+class King : Figure
+{
+    public King(int newX, int newY) : base(newX, newY)
+    {
+    }
+
+    public override bool Move(int newX, int newY)
+    {
+        if (Math.Abs(X - newX) <= 1 && Math.Abs(Y - newY) <= 1)
+        {
+            X = newX;
+            Y = newY;
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
