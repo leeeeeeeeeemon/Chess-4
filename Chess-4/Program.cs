a79a42a8fb0a78fd64cc751ec890b671d09b90f7
Барышев Эмиль Евгеньевич <211904@mck-ktits.ru>
2021-05-22 14:09:14 +0300

    Добавьте файлы проекта.

---
+++ Chess-4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chess_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
